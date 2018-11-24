b9c122a7c4a3336169dc6b7d2ce20aec4be883a7
amoinier <amoinier@outlook.fr>
2018-11-24 11:56:09 +0100

    reinstall

--- javascripts/tools/utils.js
+++ javascripts/tools/utils.js
@@ -3,10 +3,13 @@ module.exports = {
         return path.substr(path.lastIndexOf('/') + 1, path.length)
     },
     wordLetterUppercase: (text) => {
+        if (!text)
+            return null
+
         text = text.toLowerCase()
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-        .join(' ');
+        .join(' ')
 
         return text
     },
